1b540d2d68ef594f918320470f66c3c30a70d066
Sergii Gorodeikin <55316965+iEnchanter@users.noreply.github.com>
2025-06-08 00:20:54 +0200

    src/js/markup-artists-functions.js

--- src/js/markup-artists-functions.js
+++ src/js/markup-artists-functions.js
@@ -1,2 +1,41 @@
-// тут мають бути фннкції по розмітці карток артистів для секції артисти. 
-// Та по розмітці картки для модального вікна.
+import { refs } from './refer.js';
+
+export function createArtistCard(artist) {
+  const {
+    _id: id,
+    strArtist: name,
+    strArtistThumb: photo,
+    genres = [],
+    strBiographyEN: shortDescription,
+  } = artist;
+
+  const genresMarkup =
+    `<ul class="artist-genres-list">` +
+    genres.map(genre => `<li class="artist-genre-item">${genre}</li>`).join('') +
+    `</ul>`;
+
+  return `
+    <li class="artist-card" data-id="${id}">
+      <img src="${photo}" alt="${name}" class="artist-photo" />
+      <h4 class="artist-name epilogue">${name}</h4>
+      ${genresMarkup}
+      <p class="artist-description">
+        ${shortDescription ? shortDescription.slice(0, 90) + '...' : 'No description available.'}
+      </p>
+      <div class="learn-more-container" data-id="${id}">
+        <span class="learn-more-text">Learn More</span>
+        <svg class="learn-more-icon" viewBox="0 0 31 32">
+          <use href="../img/icons.svg#icon-right-caret-learn-more"></use>
+        </svg>
+      </div>
+    </li>
+  `;
+}
+export function renderArtists(artistArray) {
+    const markup = artistArray.map(createArtistCard).join('');
+    refs.artistsList.insertAdjacentHTML('beforeend', markup);
+}
+
+export function clearArtistsList() {
+    refs.artistsList.innerHTML = '';
+}
