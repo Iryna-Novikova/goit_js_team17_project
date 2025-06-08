11f58268be19aa82e624ab6aade0599f2020fa37
Volodymyr <sainchukvova2104@gmail.com>
2025-06-08 21:10:13 +0300

    make header

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,11 @@
+const dataHeaderBurger = document.querySelector('.burger-btn');
+const dataModalHeader = document.querySelector('.mobile-menu');
+const dataBtnHeaderModalClose = document.querySelector('.modal-close');
 
+dataHeaderBurger.addEventListener('click', () => {
+  dataModalHeader.classList.add('is-shown');
+});
+
+dataBtnHeaderModalClose.addEventListener('click', () => {
+  dataModalHeader.classList.remove('is-shown');
+});
