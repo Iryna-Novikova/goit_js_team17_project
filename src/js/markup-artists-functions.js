40b3e9b58c5d3afb58e223824251b297ce14c1dc
Sergii Gorodeikin <55316965+iEnchanter@users.noreply.github.com>
2025-06-08 18:36:10 +0200

    Update markup-art-detail-functions.js

--- src/js/markup-artists-functions.js
+++ src/js/markup-artists-functions.js
@@ -1,7 +1,7 @@
 // тут мають бути фннкції по розмітці карток артистів для секції артисти. 
 import { refs } from './refer.js';
 
-export function createArtistCard(artist) {
+function getArtistCard(artist) {
   const {
     _id: id,
     strArtist: name,
@@ -12,28 +12,28 @@ export function createArtistCard(artist) {
 
   const genresMarkup =
     `<ul class="artists-genres-list">` +
-    genres.map(genre => `<li class="artists-genre-item">${genre}</li>`).join('') +
+    genres.map(genre => `<li class="artists-genre-item">${genre}</li>`).join(' ') +
     `</ul>`;
 
   return `
-    <li class="artists-card" data-id="${id}">
+    <li class="artists-card">
       <img src="${photo}" alt="${name}" class="artist-photo" />
-      <h4 class="artists-name epilogue">${name}</h4>
       ${genresMarkup}
+      <h4 class="artists-name epilogue">${name}</h4>
       <p class="artists-description">
         ${shortDescription ? shortDescription.slice(0, 90) + '...' : 'No description available.'}
       </p>
       <button class="learn-more-btn" data-id="${id}" type="button">
         <span class="learn-more-text">Learn More</span>
-        <svg class="learn-more-icon" viewBox="0 0 31 32">
-          <use href="./img/icons.svg#icon-right-caret-learn-more"></use>
+        <svg class="learn-more-icon" viewBox="0 0 31 32" width="24" hight="24">
+          <use href="/img/icons.svg#icon-right-caret-learn-more"></use>
         </svg>
       </button>
     </li>
   `;
 }
 export function renderArtists(artistArray) {
-    const markup = artistArray.map(createArtistCard).join('');
+    const markup = artistArray.map(getArtistCard).join('');
     refs.artistsList.insertAdjacentHTML('beforeend', markup);
 }
 
