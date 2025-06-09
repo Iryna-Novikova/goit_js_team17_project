26c105b4cd20f04e7ee250d8b3772a6e7bcaada7
Serge <55316965+Gorodeikin@users.noreply.github.com>
2025-06-09 23:57:14 +0200

    Update markup-artists-functions.js

--- src/js/markup-artists-functions.js
+++ src/js/markup-artists-functions.js
@@ -15,14 +15,15 @@ function createArtistCard(artist) {
     genres.map(genre => `<li class="artists-genre-item">${genre}</li>`).join(' ') +
     `</ul>`;
 
+  const descriptionText = shortDescription
+    ? shortDescription.slice(0, 90) + '...' : 'No description available.';
+  
   return `
     <li class="artists-card">
       <img src="${photo}" alt="${name}" class="artist-photo" />
       ${genresMarkup}
       <h4 class="artists-name epilogue">${name}</h4>
-      <p class="artists-description">
-        ${shortDescription ? shortDescription.slice(0, 90) + '...' : 'No description available.'}
-      </p>
+      <p class="artists-description">${descriptionText}</p>
       <button class="learn-more-btn" data-id="${id}" type="button">
         <span class="learn-more-text">Learn More</span>
         <svg class="learn-more-icon" viewBox="0 0 31 32" width="24" height="24">
