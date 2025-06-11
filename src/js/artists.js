d5999260808276291f2c663fcca7c9607416bed1
Alena <aliona333.com@gmail.com>
2025-06-11 18:36:29 +0200

    add artits.js correction

--- src/js/artists.js
+++ src/js/artists.js
@@ -1,55 +1,49 @@
 import { refs } from './refer.js';
 import { createArtistsList } from './markup-artists-functions.js';
-import { showLoadMoreBtn, hideLoadMoreBtn,  showLoaderArtist,  hideLoaderArtist } from './show-hide-functions.js';
-import { getArtist } from './artists-api.js'; 
-
-
+import { showLoadMoreBtn, hideLoadMoreBtn,  showLoaderArtists,  hideLoaderArtists } from './show-hide-functions.js';
+import { getArtists } from './artists-api.js';
+import { openArtistModal } from './artist-detail-modal';
 
 let currentPage = 1;
 const limit = 8;
-let allArtists = []; 
+let totalPages;
 
+loadArtistsList();
 
-async function hndLoadMoreClick() {
-    try {
-        hideLoadMoreBtn();
-        showLoaderArtist();
-
-        const data = await getArtist(currentPage, limit);
+async function loadArtistsList () {
+  try {
+      hideLoadMoreBtn();
+      showLoaderArtists();
+        const data = await getArtists(currentPage, limit);
         const newArtists = data.artists;
-        const totalFetchedArtists = data.totalArtists;
-
-        allArtists = [...allArtists, ...newArtists];
+    const totalFetchedArtists = data.totalArtists;
+        // allArtists = [...allArtists, ...newArtists];
         createArtistsList(newArtists);
-
-        totalPages = Math.ceil(totalFetchedArtists / limit);
-
+        totalPages = Math.ceil (totalFetchedArtists / limit);
         if (currentPage < totalPages) {
             showLoadMoreBtn();
             currentPage++;
         } else {
             hideLoadMoreBtn();
         }
-
     } catch (error) {
-        console.error('Помилка завантаження артистів:', error);
-    } 
-      hideLoaderArtist();
+    refs.artistsListElm.innerHTML = `Помилка завантаження артистів: ${error}`
+  };
+      hideLoaderArtists();
 }
-
-refs.loadMoreBtn.addEventListener('click', hndLoadMoreClick);
-
-refs.artistsList.addEventListener('click', e => {
+function hndLoadMoreClick() {
+  loadArtistsList();
+}
+refs.loadMoreBtnElm.addEventListener('click', hndLoadMoreClick);
+refs.artistsListElm.addEventListener('click', e => {
   const learnMoreBtn = e.target.closest('.learn-more-btn');
   if (!learnMoreBtn) {
     return;
   }
-
   const artistId = learnMoreBtn.dataset.id;
   if (!artistId) {
     console.warn('Не знайдено data-id на кнопці "Learn More"');
     return;
   }
-
-  getArtist(artistId, dataset.genres);
+  openArtistModal(artistId, learnMoreBtn.dataset.genres);
 });
