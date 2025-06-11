e3e071bf76eb814df22afd199a8cd149bf711b9a
Daryna Diadiuk|uni <darinadyadyuk11@gmail.com>
2025-06-11 11:17:53 +0300

    fix-refer

--- src/js/refer.js
+++ src/js/refer.js
@@ -1,42 +1,29 @@
-// тут потрібно створити об'єкт refer усіх елементів документу.
-//  Приклади були в лекціях викладача.
-
-// Приклад використання "import { refs } from '../js/refer.js';" наприклад, додати слухач на поле пошуку refs.searchInput.addEventListener('input', ()=>{});
-
 export const refs = {
   // header
-  dataHeaderBurger: document.querySelector('.burger-btn'),
-  dataBtnHeaderModalClose: document.querySelector('.close-menu-btn'),
+  headerBurger: document.querySelector('[data-menu-open]'),
+  modalHeaderCloseBtn: document.querySelector('.close-menu-btn'),
 
-  dataModalHeader: document.querySelector('[data-menu]'), // мобільне меню
+  modalHeader: document.querySelector('[data-menu]'), // мобільне меню
   logoLinkMenu: document.querySelector('.logo-link'), // логотип в моб. меню
   mobileNavLinks: document.querySelector('.mobile-nav-links'), // посилання в моб. меню
 
-  // hero
-  heroBtnEl: document.querySelector('.hero-nav-btn'), // кнопка "Explore Artists"
-
   // Artists Section
-
   artistsList: document.querySelector('[data-artist-gallery]'), // Список карток артистів
-  leanMoreBtn: document.querySelector('.learn-more-btn'), // Кнопка "Learn More" на картці артиста
-  loadMoreBtn: document.querySelector('[data-btn-load-more]'), // Кнопка "Load More"
+  loaderBtn: document.querySelector('[data-btn-load-more]'), // Кнопка "Load More"
   artistLoadEl: document.querySelector('[data-artist-loader]'),
   artistInfoElm: document.querySelector('[data-artist-info]'),
 
   // Artist Details Modal
-  artistsModalBackdrop: document.querySelector('[data-artist-detail-loader]'), // Модальне вікно
+  artistsModalBackdrop: document.querySelector('[data-artists-modal]'), // Модальне вікно
   artistsModalCloseBtn: document.querySelector('.artists-modal-close-btn'), // Кнопка закриття модального вікна
-  artistsModalContentWrapper: document.querySelector(
-    '.artist-modal-content-wrapper'
-  ), // Контейнер, куди вставлятиметься контент модалки
 
   // Feedback Section
 
-  feedbackSliderContainer: document.querySelector('.mySwiper'), // Контейнер Swiper
   feedbackSliderPrevBtn: document.querySelector('.swiper-button-prev'), // Кнопки навігації слайдера
   feedbackSliderNextBtn: document.querySelector('.swiper-button-next'), // Кнопки навігації слайдера
   feedbackSliderPagination: document.querySelector('.swiper-pagination'), // Пагінація
   openFeedbackBtn: document.querySelector('.leave-feedback-btn'), // Кнопка відкриття форми фідбеку
+  feedbackLoaderElm: document.querySelector('[data-feedback-loader]'),
 
   // Feedback Modal
   feedbackFormBackdrop: document.querySelector('[data-feedback-form]'), // Модалка з формою фідбеку
