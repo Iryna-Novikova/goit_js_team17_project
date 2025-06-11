a6be02acc45a4f87b6a538a71e0b214b50fd7f55
Nataliia <natashazaremba82@gmail.com>
2025-06-11 06:48:51 +0300

    addloaders

--- src/js/show-hide-functions.js
+++ src/js/show-hide-functions.js
@@ -1,23 +1,5 @@
 import { refer } from './js/refer.js';
-// --- Header Functions ---
-export function showMobileMenu() {
-  refs.mobileMenu.classList.remove('visually-hidden');
-}
 
-export function hideMobileMenu() {
-  refs.mobileMenu.classList.add('visually-hidden');
-}
-
-// --- Hero Functions ---
-export function showHeroButton() {
-  refs.heroBtnEl.classList.remove('visually-hidden');
-}
-
-export function hideHeroButton() {
-  refs.heroBtnEl.classList.add('visually-hidden');
-}
-
-// --- Artists Section Functions ---
 export function showLoadMoreBtn() {
   refs.loadMoreBtn.classList.remove('visually-hidden');
 }
@@ -33,57 +15,24 @@ export function showArtistsList() {
 export function hideArtistsList() {
   refs.artistsList.classList.add('visually-hidden');
 }
-
-export function showLearnMoreButton() {
-  refs.leanMoreBtn.classList.remove('visually-hidden');
+export function showLoaderFeedback() {
+  refs.feedbackLoaderElm.classList.remove('visually-hidden');
 }
 
-export function hideLearnMoreButton() {
-  refs.leanMoreBtn.classList.add('visually-hidden');
+export function hideLoaderFeedback() {
+  refs.feedbackLoaderElm.classList.add('visually-hidden');
+}
+export function showLoaderArtists() {
+  refs.artistLoaderElm.classList.remove('visually-hidden');
 }
 
-// --- Artist Details Modal Functions ---
-export function showArtistsModal() {
-  refs.artistsModalBackdrop.classList.remove('visually-hidden');
-  document.body.style.overflow = 'hidden'; // Заборонити прокрутку фону
+export function hideLoaderArtists() {
+  refs.artistLoaderElm.classList.add('visually-hidden');
+}
+export function showLoaderModal() {
+  refs.artistModalLoaderElm.classList.remove('visually-hidden');
 }
 
-export function hideArtistsModal() {
-  refs.artistsModalBackdrop.classList.add('visually-hidden');
-  document.body.style.overflow = ''; // Дозволити прокрутку фону
-}
-
-// --- Feedback Modal Functions ---
-export function showFeedbackFormModal() {
-  refs.feedbackFormBackdrop.classList.remove('visually-hidden');
-  document.body.style.overflow = 'hidden';
-}
-
-export function hideFeedbackFormModal() {
-  refs.feedbackFormBackdrop.classList.add('visually-hidden');
-  document.body.style.overflow = '';
-}
-
-export function showFeedbackSlider() {
-  refs.feedbackSliderContainer.classList.remove('visually-hidden');
-}
-
-export function hideFeedbackSlider() {
-  refs.feedbackSliderContainer.classList.add('visually-hidden');
-}
-
-export function showFeedbackSliderPrevBtn() {
-  refs.feedbackSliderPrevBtn.classList.remove('visually-hidden');
-}
-
-export function hideFeedbackSliderPrevBtn() {
-  refs.feedbackSliderPrevBtn.classList.add('visually-hidden');
-}
-
-export function showFeedbackSliderNextBtn() {
-  refs.feedbackSliderNextBtn.classList.remove('visually-hidden');
-}
-
-export function hideFeedbackSliderNextBtn() {
-  refs.feedbackSliderNextBtn.classList.add('visually-hidden');
+export function hideLoaderModal() {
+  refs.artistModalLoaderElm.classList.add('visually-hidden');
 }
