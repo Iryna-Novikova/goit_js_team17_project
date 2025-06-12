519f32029104c2409842da7b22947e0c79ed45cf
Volodymyr <sainchukvova2104@gmail.com>
2025-06-12 13:48:10 +0300

    change header to header-section-2

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,40 @@
+import { refs } from './refer';
 
+function showMobileMenu() {
+  refs.modalHeader.classList.add('is-shown-menu-header');
+  document.addEventListener('keydown', onEscPress);
+  refs.modalHeader.addEventListener('click', onBackdropClick);
+  refs.modalHeaderCloseBtn.addEventListener('click', hideMobileMenu);
+  refs.mobileNavLinks.addEventListener('click', onNavClick);
+  document.body.classList.add('no-scroll');
+}
+
+function hideMobileMenu() {
+  refs.modalHeader.classList.remove('is-shown-menu-header');
+  document.removeEventListener('keydown', onEscPress);
+  refs.modalHeader.removeEventListener('click', onBackdropClick);
+  refs.modalHeaderCloseBtn.removeEventListener('click', hideMobileMenu);
+  refs.mobileNavLinks.removeEventListener('click', onNavClick);
+  document.body.classList.remove('no-scroll');
+}
+
+function onEscPress(e) {
+  if (e.key === 'Escape') {
+    hideMobileMenu();
+  }
+}
+
+function onBackdropClick(e) {
+  if (e.currentTarget === e.target) {
+    hideMobileMenu();
+  }
+}
+
+function onNavClick(e) {
+  if (e.target.tagName === 'A') {
+    hideMobileMenu();
+  }
+}
+
+refs.headerBurger.addEventListener('click', showMobileMenu);
+refs.modalHeaderCloseBtn.addEventListener('click', hideMobileMenu);
