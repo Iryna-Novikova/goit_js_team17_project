3599e48db611dc6cf64108a47ecfcec538af73c4
Yevhen Shymka <schimka3@gmail.com>
2025-06-12 14:45:45 +0200

    Your commit message

--- src/js/markup-art-detail-functions.js
+++ src/js/markup-art-detail-functions.js
@@ -1,11 +1,11 @@
 import { refs } from './refer.js';
 
-export function createArtistCard(artist, genres) {
-  artistCardMarkup = artistCardMarkup(artist, genres);
-  refs.artistInfoElm.innerHTML = artistCardMarkup;
+export function createArtistCard(artist) {
+  const markup = artistCardMarkup(artist);
+  refs.artistInfoElm.innerHTML = markup;
 }
 
-function artistCardMarkup(artist, genres) {
+export function artistCardMarkup(artist) {
   const {
     strArtist: name,
     strArtistThumb: photo,
@@ -16,65 +16,53 @@ function artistCardMarkup(artist, genres) {
     strCountry: country,
     strBiographyEN: biography,
     albumsList = [],
+    genre = [],
   } = artist;
 
-  const genresListMarkup =
-    `<ul class="artists-genres-list">` +
-    genres
-      .map(genre => `<li class="artists-genre-item">${genre}</li>`)
-      .join(' ') +
-    `</ul>`;
+  const genresListMarkup = `
+    <ul class="artists-genres-list">
+      ${genres
+        .map(genre => `<li class="artists-genre-item">${genre}</li>`)
+        .join(' ')}
+    </ul>`;
 
   const albumsListMarkup = albumsList
     .map(album => createAlbumListMarkup(album))
     .join(' ');
 
   let yearsText = formedYear;
-  diedYear ? (yearsText += '-' + diedYear) : (yearsText += '-present');
+  yearsText += diedYear ? `-${diedYear}` : `-present`;
 
-  return `<h5 class="epilogue artist-name">${name}</h5>
+  return `
+    <h5 class="epilogue artist-name">${name}</h5>
     <div class="artist-info">
       <img class="artist-photo" src="${photo}" alt="${name} photo" />
       <div class="artist-about">
-      <div class="artist-category-info>
-        <p class="artist-category-name">
-        Years active
-        </p>
-        <p class="artist-category-text">
-        ${yearsText}
-        </p>
+      <div class="artist-about-part">
+        <div class="artist-category-info artist-about-part-left">
+          <p class="artist-category-name">Years active</p>
+          <p class="artist-category-text">${yearsText}</p>
+        </div>
+        <div class="artist-category-info">
+          <p class="artist-category-name">Sex</p>
+          <p class="artist-category-text">${gender}</p>
+        </div>
       </div>
-      <div class="artist-category-info>
-        <p class="artist-category-name">
-        Sex
-        </p>
-        <p class="artist-category-text">
-        ${gender}
-        </p>
-      </div>
-       <div class="artist-category-info>
-        <p class="artist-category-name">
-        Members
-        </p>
-        <p class="artist-category-text">
-        ${members}
-        </p>
-      </div>
-      <div class="artist-category-info>
-        <p class="artist-category-name">
-        Country
-        </p>
-        <p class="artist-category-text">
-        ${country}
-        </p>
-      </div>
-        <p class="artist-category-name">
-        Biography
-        </p>
-        <p class="artist-category-text-biography">
-        ${biography}
-        </p>
-       ${genresListMarkup}
+      <div class="artist-about-part ">
+        <div class="artist-category-info artist-about-part-left">
+          <p class="artist-category-name">Members</p>
+          <p class="artist-category-text">${members}</p>
+        </div>
+        <div class="artist-category-info">
+          <p class="artist-category-name">Country</p>
+          <p class="artist-category-text">${country}</p>
+        </div>
+       </div>
+        <div class="biography-box">
+        <p class="artist-category-name">Biography</p>
+        <p class="artist-category-text-biography">${biography}</p>
+        </div>
+        ${genresListMarkup}
       </div>
     </div>
     <p class="albums-header">Albums</p>
@@ -86,7 +74,7 @@ function createAlbumListMarkup(album) {
   const { strAlbum: albumName, tracks = [] } = album;
 
   const tracksListMarkup = tracks
-    .map(track => crateTrackListMarkup(track))
+    .map(track => createTrackListMarkup(track))
     .join('');
 
   return `<li>
@@ -102,49 +90,37 @@ function createAlbumListMarkup(album) {
   </li>`;
 }
 
-function crateTrackListMarkup({
+function createTrackListMarkup({
   strTrack: trackTitle,
   intDuration: time,
   movie,
 }) {
   const trackTime = getTime(time);
-  let movieSrc;
-  let movieHidden;
-  if (movie) {
-    movieSrc = movie;
-    movieHidden = ' ';
-  } else {
-    movieSrc = ' ';
-    movieHidden = ' visually-hidden';
-  }
+  const movieSrc = movie || '';
+  const movieHidden = movie ? '' : ' movie-hidden';
 
-  return `<li class="track-item>
-        <p class="track-tittle>${trackTitle}</p>
-        <p class="track-time">${trackTime}</p>
-          <a
-            class="track-movie${movieHidden}"
-            href="${movieSrc}"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          <svg class="modal-youtube-icon" width="24px" height="24px">
-            <use href="/img/icons.svg#icon-youtube"></use>
-           </svg>
-          </a>
-        </li> `;
+  return `
+    <li class="track-item">
+      <p class="track-title">${trackTitle}</p>
+      <p class="track-time">${trackTime}</p>
+      <a class="track-movie${movieHidden}" href="${movieSrc}" target="_blank" rel="noopener noreferrer">
+        <svg class="modal-youtube-icon" width="24px" height="24px">
+          <use href="/img/icons.svg#icon-youtube"></use>
+        </svg>
+      </a>
+    </li>`;
 }
 
 function getTime(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
 
-  // Remaining minutes
   const minutes = Math.floor(ms / minute);
-  // Remaining seconds
   const seconds = Math.floor((ms % minute) / second);
 
   return (
-    minutes.toString().padStart(2, 0) + ':' + seconds.toString().padStart(2, 0)
+    minutes.toString().padStart(2, '0') +
+    ':' +
+    seconds.toString().padStart(2, '0')
   );
 }
